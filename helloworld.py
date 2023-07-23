6448c04786b3afdf9341a4278c1b027302e04906
NomsuBen <olaniranbenjamin@.com>
2023-07-23 06:29:36 +0100

    first commit

---
+++ helloworld.py
@@ -0,0 +1,36 @@
+print("Hello world - I am loving ISWE")
+#print("Benjamin Olaniran")
+print("3")
+
+#This is a single line comment
+print("This is lovely")
+
+"""
+Ire is a fine boy
+"""
+
+#Variables - are use to store data
+# = is an assignment symbol 
+# Variables are case sensitive like customer, CUSTOMER, Customer and cUstomer are not the same because they are case sensitive
+
+name = "Benjamin Olaniran"
+age = 33
+location = "Oyo"
+country = "Nigeria"
+
+learner = "Faith Olaniran"
+
+customer = "Buyeke"
+amount = 60
+height = 20.1
+
+print(type(customer))
+print(type(amount))
+print(type(height))
+
+CUSTOMER = "Joel"
+product = "Iphone 11"
+price = 1000
+print("Customer name: "+ CUSTOMER)
+print("product name: " + product)
+print("Price: " + str(price)) #Casting is converting an interger to string
